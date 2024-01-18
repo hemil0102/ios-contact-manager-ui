7129695b246edf946154e905141cb39a5bd2f374
Hoi.Kwon <teaho177@gmail.com>
2024-01-18 13:34:15 +0900

    test:test commit

---
+++ ios-contact-manager-ui/Controller/ContactListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactListTableViewCell.swift
+//  ios-contact-manager-ui
+//
+//  Created by 권태호 on 18/01/2024.
+//
+
+import UIKit
+
+class ContactListTableViewCell: UITableViewCell {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
